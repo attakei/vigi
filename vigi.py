61b754209f278b84e8c7e523d59e3983ea0adc0a
Kazuya Takei <attakei@gmail.com>
2019-12-22 00:15:11 +0900

    Define script outline

--- vigi.py
+++ vigi.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
 __version__ = '0.1.0'
+
+
+def main():
+    # Convert from ``.gitignore`` to ``.gi``
+    # Spawn vim
+    # Convert from ``.gi`` to ``.gitignore``
+    pass
+
+
+if __name__ == '__main__':
+    main()
